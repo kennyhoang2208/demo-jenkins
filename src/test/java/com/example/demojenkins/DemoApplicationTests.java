5e7f9b123e13abbd8ba1c20485599f2c4fe31d83
Kenny Hoang <kennyhoang2208@gmail.com>
2019-05-21 15:54:51 +0700

    Add hello world api

--- src/test/java/com/example/demojenkins/DemoApplicationTests.java
+++ src/test/java/com/example/demojenkins/DemoApplicationTests.java
@@ -3,14 +3,35 @@ package com.example.demojenkins;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@LocalServerPort
+    private int port;
+
+    @Test
+    public void helloWorld() {
+        String username = "test";
+        String expected = "Hello World! Test";
+
+        when()
+            .get(String.format("http://localhost:%s/helloworld/%s", port, username))
+            .then()
+            .statusCode(is(200))
+            .body(containsString(expected));
+    }
 
 }
